3608568cac4f773369217534b8404b1a7647c778
arief_dihardja <arief@dihardjasoftware.com>
2021-03-07 21:05:30 +0700

    Initial Commit

---
+++ Shared/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  Shared
+//
+//  Created by фкшуа on 07/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
